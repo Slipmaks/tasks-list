e843dcfabf542e611e25f7612152243f00fbf1cc
Slipmaks <slipmaks@gmail.com>
2022-07-11 14:42:30 +0300

    updated component

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,23 @@
   <div>
     <List />
   </div>
+  <input
+    class="bg-teal-200 mb-4 mr-2 shadow-md rounded-md outline-teal-800 outline-1 py-2 pl-2"
+    v-model="listTitle"
+  /><button @click="addListToArray">Add list</button>
   <user-task-list></user-task-list>
 </template>
 
 <script setup>
-import List from "./components/List.vue";
 import { ref } from "vue";
-const msg = ref("Todo list Composition API");
-</script>
+import List from "./components/List.vue";
+const msg = "Todo list Composition API";
 
-<style></style>
+const listTitle = ref("");
+const listArray = ref([]);
+
+const addListToArray = () => {
+  const list = {};
+  listArray.push();
+};
+</script>
