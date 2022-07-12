26ecad899ba7f271a86d839ea68e538a6a8ca4d9
Slipmaks <slipmaks@gmail.com>
2022-07-12 13:26:04 +0300

    localStorage added, but not fixed

--- src/App.vue
+++ src/App.vue
@@ -22,30 +22,52 @@
     :id="list.id"
     :title="list.title"
     :tasks="list.tasks"
+    @addTask="addToTasks"
   ></user-task-list>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  ref,
+  watch,
+  watchEffect,
+} from "vue";
 import List from "./components/List.vue";
 const msg = "Todo list Composition API";
 
 const listTitle = ref("");
-const listArray = ref([
-  {
-    id: 1,
-    title: "Title one",
-    tasks: ["task 1", "task 2", "task 3"],
-  },
-  {
-    id: 2,
-    title: "Title two",
-    tasks: ["task 1", "task 2", "task 3"],
-  },
-]);
+const listArray = ref([]);
 
 const addListToArray = () => {
-  // const list = {};
-  listArray.push();
+  const newId = listArray.value.length + 1;
+  const titleValue = listTitle.value;
+  const list = {
+    id: newId,
+    title: titleValue,
+    tasks: [],
+  };
+  listArray.value.push(list);
+  listTitle.value = "";
 };
+
+const addToTasks = (listId, task) => {
+  const list = listArray.value.find((list) => list.id === listId);
+  list.tasks.push(task);
+  console.log(listArray.value);
+};
+
+watch(listArray.value, (newVal) => {
+  const json = JSON.stringify(newVal);
+  console.log("watch", json);
+  localStorage.setItem("list", json);
+});
+
+// onMounted(() => {
+//   const storedList = localStorage.getItem("list");
+//   console.log("onMounted: ", JSON.parse(storedList));
+//   listArray.value = JSON.parse(storedList);
+// });
 </script>
--- src/components/UserTaskList.vue
+++ src/components/UserTaskList.vue
@@ -1,15 +1,27 @@
 <template>
   <section>
     <h1>{{ title }}</h1>
+    <input type="text" class="bg-purple-400" v-model="userInput" />
+    <button @click="createNewTask(id)">add tasks</button>
     <ul>
       <li v-for="task in tasks" :key="task">{{ task }}</li>
     </ul>
   </section>
 </template>
 <script setup>
+import { ref } from "vue";
+const emit = defineEmits(["add-task"]);
+
 defineProps({
   id: Number,
   title: String,
   tasks: Array,
 });
+
+const userInput = ref("");
+
+const createNewTask = (id) => {
+  emit("add-task", id, userInput.value);
+  userInput.value = "";
+};
 </script>
