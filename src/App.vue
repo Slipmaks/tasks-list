2ae9217ed5c5cea55b42f7e04ebb34adef492cbe
Slipmaks <slipmaks@gmail.com>
2022-07-12 09:49:56 +0300

    UserTaskList component updated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
+  <!-- Header -->
+  <div class="text-center">
     <h1 class="text-cyan-700 font-extrabold text-4xl my-8 ml-8">{{ msg }}</h1>
     <hr />
   </div>
 
-  <div>
+  <!-- Old list  -->
+  <div hidden>
     <List />
   </div>
+
+  <!-- New list  -->
   <input
     class="bg-teal-200 mb-4 mr-2 shadow-md rounded-md outline-teal-800 outline-1 py-2 pl-2"
     v-model="listTitle"
-  /><button @click="addListToArray">Add list</button>
-  <user-task-list></user-task-list>
+  />
+  <button @click="addListToArray">Add list</button>
+  <user-task-list
+    v-for="list in listArray"
+    :key="list.id"
+    :id="list.id"
+    :title="list.title"
+    :tasks="list.tasks"
+  ></user-task-list>
 </template>
 
 <script setup>
@@ -20,10 +31,21 @@ import List from "./components/List.vue";
 const msg = "Todo list Composition API";
 
 const listTitle = ref("");
-const listArray = ref([]);
+const listArray = ref([
+  {
+    id: 1,
+    title: "Title one",
+    tasks: ["task 1", "task 2", "task 3"],
+  },
+  {
+    id: 2,
+    title: "Title two",
+    tasks: ["task 1", "task 2", "task 3"],
+  },
+]);
 
 const addListToArray = () => {
-  const list = {};
+  // const list = {};
   listArray.push();
 };
 </script>
--- src/components/UserTaskList.vue
+++ src/components/UserTaskList.vue
@@ -1,6 +1,15 @@
 <template>
-  <section><h1>Hello</h1></section>
+  <section>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </section>
 </template>
-<script>
-export default {};
+<script setup>
+defineProps({
+  id: Number,
+  title: String,
+  tasks: Array,
+});
 </script>
